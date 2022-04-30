0ffd4c6580ad2272acc6e4dc407b0af5e105353d
Rouhis <eeturouhiainen1@gmail.com>
2022-04-30 16:56:44 +0300

    eka commit ku vanha branch tyhjens ittensä jostai syystä...

--- app/src/main/java/com/example/terveyspaivakirja/MainActivity.java
+++ app/src/main/java/com/example/terveyspaivakirja/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.terveyspaivakirja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = (Button)findViewById(R.id.button);
 
-    }
-}
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Kalorilaskuri.class));
+                finish();
+
+            }
+        });
+}}
