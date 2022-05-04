f4e4f9d019c09cef18e63bbed0215dce653f0d27
tammipuu <tammipuu@protonmail.com>
2022-05-04 17:24:34 +0300

    MainActivity: Add date picker and app bar items (WIP)

--- app/src/main/java/com/example/terveyspaivakirja/MainActivity.java
+++ app/src/main/java/com/example/terveyspaivakirja/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.terveyspaivakirja;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment menu = new MenuFragment();
+        menu.onCreateOptionsMenu(R.menu.main_menu, getMenuInflater());
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment fragment = new DatePickerFragment();
+        fragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
---
+++ app/src/main/java/com/example/terveyspaivakirja/MenuFragment.java
@@ -0,0 +1,26 @@
+package com.example.terveyspaivakirja;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+    public MenuFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu, menu);
+    }
+}
